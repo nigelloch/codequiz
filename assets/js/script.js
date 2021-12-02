85f9fb0f3e31e2217ba4d6766ff396c90c6c9869
loch <nigelloch@gmail.com>
2021-12-02 17:55:35 -0500

    begun building js

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+var timer = document.getElementById("timer");
+
+function startGame() {
+
+}
+
+function 
+
+
+
+var questions = [
+    {
+        title: "Who is Batmans secret identity?",
+        choices: ["Clark Kent", "Peter Parker", "Tony Stark", "Bruce Wayne"],
+        answer: "Bruce Wayne"
+    },
+    {
+        title: ""
+    }
+]
