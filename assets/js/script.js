57eee45c07bb9ba270cb8de899bcedb0da04e268
loch <nigelloch@gmail.com>
2021-12-04 14:36:21 -0500

    added hide and functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,36 @@
-var timer = document.getElementById("timer");
+const startButton = document.getElementById('startbtn')
 
+const answerContainer = document.getElementById('answer-container')
+
+const quizInstructions = document.getElementById('quiz-instructions')
+
+const question = document.getElementById('question')
+
+startButton.addEventListener('click', startGame)
+
+// start timer, hide start button, show question, show current score, reset timer
 function startGame() {
+    console.log(answerContainer.classList)
+    answerContainer.classList.toggle('hide');
+
+    console.log(quizInstructions.classList)
+    quizInstructions.classList.toggle('hide');
+
+    console.log(question.classList)
+    question.classList.toggle('hide');
+
+    console.log(startButton.classList)
+    startButton.classList.toggle('hide');
 
 }
 
-function 
+function setNextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
 
 
 
-var questions = [
-    {
-        title: "Who is Batmans secret identity?",
-        choices: ["Clark Kent", "Peter Parker", "Tony Stark", "Bruce Wayne"],
-        answer: "Bruce Wayne"
-    },
-    {
-        title: ""
-    }
-]
