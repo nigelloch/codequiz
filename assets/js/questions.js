1d80ff525b578a89acb35755f665d982857917f9
loch <nigelloch@gmail.com>
2021-12-06 17:36:12 -0600

    result of meeting with tutor - Andres is awesome

---
+++ assets/js/questions.js
@@ -0,0 +1,33 @@
+var questionsList = [
+    {
+      title: 'What is 2 + 2?',
+      answers: [
+        { text: '4', correct: true },
+        { text: '22', correct: false },
+        { text: '34', correct: false },
+        { text: '32', correct: false }
+      ],
+      crcAnswer: '4'
+    },
+    {
+      title: 'What is 3 + 3?',
+      answers: [
+        { text: '6', correct: true },
+        { text: '24', correct: false },
+        { text: '332', correct: false },
+        { text: '54', correct: false }
+      ],
+      crcAnswer: '6'
+    },
+    {
+      title: 'What is 4 + 4?',
+      answers: [
+        { text: '8', correct: true },
+        { text: '2', correct: false },
+        { text: '4', correct: false },
+        { text: '6', correct: false }
+      ],
+      crcAnswer: '8'
+    },
+    
+  ]
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,69 @@
-const startButton = document.getElementById('startbtn')
+var startButton = document.getElementById('startbtn')
+var questionContainer = document.getElementById('question-container')
+var quizInstructions = document.getElementById('quiz-instructions')
+var questionIdElement = document.getElementById('questionId')
+var nextButton = document.getElementById('nextbtn')
+var questionText = document.getElementById('questionId')
+// answer buttons
+var opt1 = document.getElementById('opt1')
+var opt2 = document.getElementById('opt2')
+var opt3 = document.getElementById('opt3')
+var opt4 = document.getElementById('opt4')
 
-const answerContainer = document.getElementById('answer-container')
-
-const quizInstructions = document.getElementById('quiz-instructions')
-
-const question = document.getElementById('question')
-
-const nextButton = document.getElementById('nextbtn')
+var currentQuestion;
+var timeRemaining;
 
 startButton.addEventListener('click', startGame)
-
-nextButton.addEventListener('click', setNextQuestion)
+//nextButton.addEventListener('click', setNextQuestion)
 
 // start timer, hide start button, show question, show current score, reset timer
 function startGame() {
-    console.log(answerContainer.classList)
-    answerContainer.classList.toggle('hide');
-
-    console.log(quizInstructions.classList)
+    
     quizInstructions.classList.toggle('hide');
-
-    console.log(question.classList)
-    question.classList.toggle('hide');
-
-    console.log(startButton.classList)
     startButton.classList.toggle('hide');
-
-    console.log(nextButton.classList)
-    nextButton.classList.toggle('hide');
-
-}
-
-function setNextQuestion() {
+    questionContainer.classList.remove('hide');
+    currentQuestion= 0;
+    nextQuestions()
+    timeRemaining = 60;
+    timer()
 
 }
 
-function selectAnswer() {
+// load next question and answers
+function nextQuestions() {
+    // questionsList.classList.toggle('hide')
+    questionText.textContent = questionsList[currentQuestion].title;
+    opt1.textContent = questionsList[currentQuestion].answers[0].text;
+    opt2.textContent = questionsList[currentQuestion].answers[1].text;
+    opt3.textContent = questionsList[currentQuestion].answers[2].text;
+    opt4.textContent = questionsList[currentQuestion].answers[3].text;
 
 }
 
 
+// confirm answer correct or wrong - add 10 second penalty to timer if wrong
+function checkAnswerandIterate (event) {
+    var chosenAnswer = event.target.textContent
 
+
+    currentQuestion++;
+    if(currentQuestion<questionsList.length){
+        nextQuestions()
+    }
+}
+
+function timer () {
+    var interval= setInterval(function(){
+        //decrease time every second
+        timeRemaining--;
+        console.log(timeRemaining)
+        if(timeRemaining <= 0) {
+            clearInterval(interval)
+        }
+    }, 1000)
+}
+// game complete all questions answered - total points - save to localStorage for high score - play again option
+opt1.addEventListener("click", checkAnswerandIterate);
+opt2.addEventListener("click", checkAnswerandIterate);
+opt3.addEventListener("click", checkAnswerandIterate);
+opt4.addEventListener("click", checkAnswerandIterate);
